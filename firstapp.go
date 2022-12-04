9a327149d6e2e4f364fdf0498ca7a138c6537f97
tyange <usun16@gmail.com>
2022-12-04 22:11:37 +0900

    float64, float32, rune, byte

--- firstapp.go
+++ firstapp.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	// var greetingText string
+	// greetingText = "Hello World"
+
+	// var greetingText string = "Hello World"
+
+	greetingText := "Hello World"
+
+	fmt.Println(greetingText)
+
+	var firstFloat64 float64 = 1.123123123123123123123
+	var firstFloat32 float32 = 1.123123123123123
+
+	fmt.Println(firstFloat64)
+	fmt.Println(firstFloat32)
+
+	var firstRune rune = '&'
+	fmt.Println(firstRune)
+	fmt.Println(string(firstRune))
+
+	var firstByte byte = 'a'
+	fmt.Println(firstByte)
 }
