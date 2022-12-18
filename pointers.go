741ec69e36e137a925e300622d9af78e9b3fbff8
tyange <usun16@gmail.com>
2022-12-19 07:56:02 +0900

    get a value with *

--- pointers.go
+++ pointers.go
@@ -8,9 +8,14 @@ func main() {
 	fmt.Println(age)
 
 	myAge := &age
-	// 아스터리크(*)는 myAge 변수가 int 타입의 데이터를 가진 메모리 주소를 가르킬 것을 의미함
+	// 아스터리크(*)는 myAge 변수가 int 타입의 데이터를 가진 메모리 주소의 '값'을 가리킬 것을 의미함.
 	// var myAge *int
 	// myAge = &age
 
-	fmt.Println(myAge)
+	fmt.Println(*myAge)
+
+	*myAge = 33
+
+	fmt.Println(*myAge)
+	fmt.Println(age)
 }
