ba0a1f5c71c25cad523401d9f884dbdf6f98fcbe
tyange <usun16@gmail.com>
2022-12-26 07:48:51 +0900

    creating struct with helper function

--- structs.go
+++ structs.go
@@ -15,6 +15,19 @@ type User struct {
 	createdDate time.Time
 }
 
+func NewUser(fName string, lName string, bDate string) User {
+	created := time.Now()
+
+	user := User{
+		firstName:   fName,
+		lastName:    lName,
+		birthDate:   bDate,
+		createdDate: created,
+	}
+
+	return user
+}
+
 var reader = bufio.NewReader(os.Stdin)
 
 func main() {
@@ -23,14 +36,8 @@ func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
-	created := time.Now()
 
-	newUser = User{
-		firstName:   firstName,
-		lastName:    lastName,
-		birthDate:   birthdate,
-		createdDate: created,
-	}
+	newUser = NewUser(firstName, lastName, birthdate)
 
 	fmt.Println(newUser)
 }
