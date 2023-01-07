1b8d3e826596e9bcdcbfe6d9f8b9a991d1f12272
tyange <usun16@gmail.com>
2023-01-07 19:10:15 +0900

    slice 3

--- lists.go
+++ lists.go
@@ -3,24 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	var productNames [4]string
-	productNames = [4]string{"a Book"}
-	prices := [4]float64{10.99, 40.0, 12.99, 20.0}
+	prices := []float64{10.99, 8.99}
+	fmt.Println(prices[1])
+	prices[1] = 9.99
+
+	prices = append(prices, 5.99)
+	prices = prices[1:]
 	fmt.Println(prices)
-
-	productNames[2] = "a Carpet"
-
-	fmt.Println(productNames)
-
-	fmt.Println(prices[2])
-
-	featuredPrices := prices[:3]
-	featuredPrices[0] = 199.99
-	highlightedPrices := featuredPrices[:1]
-	fmt.Println(highlightedPrices)
-	fmt.Println(prices)
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
-
-	highlightedPrices = highlightedPrices[:3]
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
 }
+
+//func main() {
+//	var productNames [4]string
+//	productNames = [4]string{"a Book"}
+//	prices := [4]float64{10.99, 40.0, 12.99, 20.0}
+//	fmt.Println(prices)
+//
+//	productNames[2] = "a Carpet"
+//
+//	fmt.Println(productNames)
+//
+//	fmt.Println(prices[2])
+//
+//	featuredPrices := prices[:3]
+//	featuredPrices[0] = 199.99
+//	highlightedPrices := featuredPrices[:1]
+//	fmt.Println(highlightedPrices)
+//	fmt.Println(prices)
+//	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+//
+//	highlightedPrices = highlightedPrices[:3]
+//	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+//}
